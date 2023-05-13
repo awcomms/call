58cf15b3e5de2e42f9c2ca0453a6723a1ec57bb8
EdwardGold Edem Hogan <edge37@outlook.com>
2023-05-13 13:40:33 +0100

    adapter-node

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
 	import { PUBLIC_PEER_SERVER } from '$env/static/public';
 	import { v4 } from 'uuid';
 	import type { MediaConnection } from 'peerjs';
+	import { stringStore } from '$lib/util/store';
 
 	$: if (ref && remote_stream) ref.srcObject = remote_stream;
 
@@ -22,8 +23,29 @@
 	onMount(() => {
 		navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then((stream) => {
 			local_stream = stream;
-			import('peerjs').then(({ default: Peer }) => {
+			import('peerjs').then(async ({ default: Peer }) => {
 				peer = new Peer();
+
+				// check if this peer's id exists in pinecone in the call namespace
+				// CHECK IF THIS WORKS
+				await axios
+					.post('/pinecone', {
+						act: 'fetch',
+						arg: [[peer.id], 'call']
+					})
+					.then(async ({ data }) => {
+						if (!Object.keys(data.vectors).length) {
+							// if it doesn't, create a new id in pinecone with an empty embedding
+							await axios.post('/embedding', '').then(async ({ data: values }) => {
+								await axios.post('/pinecone', {
+									act: 'update',
+									arg: { updateRequest: { id: peer.id, values, namespace: 'call' } }
+								});
+							});
+						}
+					});
+				// edit embedding of this id in pinecone to be of an empty string
+
 				peer.on('call', (c: MediaConnection) => {
 					if (c.peer === saved_target) {
 						c.answer(stream);
@@ -38,37 +60,45 @@
 		});
 	});
 
-	const call = (id: string, stream: MediaStream) => {
+	const search = async (vector: number[]) => {
 		if (!should_call) return;
-		let c = peer.call(id, stream);
-		c.on('stream', (s) => {
-			remote_stream = s;
-		}).on('close', () => {
-			call(id, stream);
-		});
+		await axios
+			.post('/pinecone', {
+				act: 'query',
+				arg: { queryRequest: { topK: 1, vector, includeValues: true, namespace: 'call' } }
+			})
+			.then(({ data }) => {
+				if (!data.matches.length) return alert('no matches');
+				saved_target = data.matches[0].id;
+				peer
+					.call(peer.id, local_stream)
+					.on('stream', (s) => {
+						remote_stream = s;
+					})
+					.on('close', () => {
+						search(vector);
+					});
+			});
 	};
 
-	const search = async () => {
+	const description_change = async () => {
 		should_call = false;
 		await axios.post('/embedding', description).then(async ({ data: vector }) => {
-			await axios
-				.post('/pinecone', {
-					act: 'query',
-					arg: { queryRequest: { topK: 3, vector, includeValues: true, namespace: 'call' } }
-				})
-				.then(({ data }) => {
-					if (!data.matches.length) return console.log('no matches');
-					saved_target = data.matches[0].id;
-					should_call = true;
-					call(data.matches[0].id, local_stream);
-				});
+			// edit id in pinecone with this embedding
+			await axios.post('/pinecone', {
+				act: 'update',
+				arg: { updateRequest: { id: peer.id, values: vector, namespace: 'call' } }
+			});
+			should_call = true;
+			// query pinecone for first match
+			search(vector);
 		});
 	};
 </script>
 
 <Modal bind:open={description_open} passiveModal modalHeading="Edit description">
 	<TextInput bind:value={description} labelText="Description" />
-	<Button on:click={search}>Set</Button>
+	<Button on:click={description_change}>Set</Button>
 </Modal>
 
 <Button on:click={() => (description_open = true)}>edit description</Button>
--- src/routes/pinecone/+server.ts
+++ src/routes/pinecone/+server.ts
@@ -2,25 +2,32 @@ import { index } from '$lib/pinecone';
 import { json, error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-type Act = '_delete' | 'describeIndexStats' | 'fetch' | 'query' | 'update' | 'upsert';
+type Act = 'delete1' | 'describeIndexStats' | 'fetch' | 'query' | 'update' | 'upsert';
 
 export const POST = (async ({ request }) => {
 	const req = await request.json();
-	console.log('req: ', req)
+	console.log('req: ', req);
 	const act: Act = req.act;
-	return await index[act](req.arg)
+	let rqe;
+	if (act === 'fetch' || 'delete1') {
+		console.log('fd', act)
+		rqe = index[act](...req.arg);
+	} else {
+		rqe = index[act](req.arg);
+	}
+	return await rqe
 		.then((res) => {
 			console.log('res: ', res);
-			// if (res.statusText !== 'OK') {
-			// 	console.log('error*: ', res.data)
-			// 	throw error(500, JSON.stringify(res.data));
-			// }
-			console.log('sr', res)
+			if (res.statusText !== 'OK') {
+				console.log('error*: ', res.data);
+				throw error(500, JSON.stringify(res.data));
+			}
+			console.log('sr', res);
 			return json(res);
 		})
-		// .catch((e) => {
-		// 	console.log('error: ', `\`\`\`${e}\`\`\``);
-		// 	console.dir('req: ', `\`\`\`${req}\`\`\``);
-		// 	throw error(500, 'Internal Server Error');
-		// })
+		.catch((e) => {
+			console.log('fetch error: ', `\`\`\`${e}\`\`\``);
+			console.dir('fetch error: req: ', `\`\`\`${req}\`\`\``);
+			throw error(500, JSON.stringify(e));
+		});
 }) satisfies RequestHandler;
