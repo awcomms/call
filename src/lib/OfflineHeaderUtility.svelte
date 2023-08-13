18ec157b668e52cbef51733e3d666d9699d4eee6
EdwardGold Edem Hogan <edge37@outlook.com>
2023-08-13 16:16:04 +0000

    .

---
+++ src/lib/OfflineHeaderUtility.svelte
@@ -0,0 +1,9 @@
+<script>
+	import ConnectionSignalOff from 'carbon-icons-svelte/lib/ConnectionSignalOff.svelte';
+	import { HeaderGlobalAction } from 'carbon-components-svelte';
+	import { offline } from './stores';
+</script>
+
+{#if $offline}
+    <HeaderGlobalAction aria-label="No internet connection" icon={ConnectionSignalOff} />
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,16 @@
 <script>
 	import Offline from '$lib/Offline.svelte';
-	import { offline } from '$lib/stores';
-	import { HeaderAction } from 'carbon-components-svelte';
-	import ConnectionSignalOff from 'carbon-icons-svelte/lib/ConnectionSignalOff.svelte';
+	import OfflineHeaderUtility from '$lib/OfflineHeaderUtility.svelte';
 
 	import { Layout, VercelAnalyticsInject } from 'sveltekit-carbon-utils';
-
-	$: isOpen = false;
 </script>
 
 <Offline />
 <VercelAnalyticsInject />
 <Layout platformName="vidchat" company="">
-	{#if $offline}
-		<!-- <svelte:fragment slot="header-utilities">
-			<HeaderAction
-				bind:isOpen
-				icon={ConnectionSignalOff}
-				closeIcon={ConnectionSignalOff}
-				on:open={() => (isOpen = false)}
-			/>
-		</svelte:fragment> -->
-	{/if}
+		<svelte:fragment slot="header-utilities">
+			<OfflineHeaderUtility />	
+		</svelte:fragment>
 	<svelte:fragment slot="content-grid">
 		<slot />
 	</svelte:fragment>
