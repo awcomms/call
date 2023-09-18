c667b5535df1f7d59de478e5c3fad6552eec39d2
EdwardGold Edem Hogan <edge37@outlook.com>
2023-09-18 22:32:38 +0000

    refactored update function

--- src/routes/users/[id]/+server.ts
+++ src/routes/users/[id]/+server.ts
@@ -3,7 +3,7 @@ import type { RequestHandler } from './$types';
 // import { openai } from '$lib/openai';
 import { client } from '$lib/redis';
 // import { writeFileSync } from 'fs';
-import {float32_buffer} from 'sveltekit-carbon-utils'
+import { float32_buffer } from 'sveltekit-carbon-utils';
 import { handle_server_error } from '$lib/handle_server_error';
 import { openai } from '$lib/openai';
 import { dev } from '$app/environment';
@@ -28,14 +28,11 @@ export const PUT: RequestHandler = async ({ params, request }) => {
 	try {
 		let id = PREFIX.concat(params.id);
 		const args = await request.json();
-		// const text = args.text;
-		// delete args.text;
-		if (args.text && dev) {
+		if (args.text) {
 			const v = await openai.createEmbedding({ model: embedding_model, input: args.text });
-			args.v = float32_buffer(v.data.data[0].embedding)
+			args.v = float32_buffer(v.data.data[0].embedding);
 			// writeFileSync('sample_embedding.json', JSON.stringify(v.data));
 		}
-		// console.log(args);
 		if (!(await client.exists(id))) {
 			client.json.set(id, '$', args);
 		} else {
@@ -43,10 +40,8 @@ export const PUT: RequestHandler = async ({ params, request }) => {
 				await client.json.set(id, `$.${key}`, args[key]);
 			}
 		}
-		// console.log('e', id, await client.ft.search(index, "*"))
+		return new Response();
 	} catch (e: any) {
 		throw handle_server_error('PUT users/{id} error: ', e);
 	}
-
-	return new Response();
 };
